91edfcafce6f2092b2d1ed91557ca5c96e9570a5
János Csordás <janoscsordas072@gmail.com>
2024-10-04 18:54:50 +0200

    dev

--- hollovar/app/layout.tsx
+++ hollovar/app/layout.tsx
@@ -19,13 +19,13 @@ export default function RootLayout({
         className={`${GeistSans.className} antialiased`}
       >
         {/* navigation wrapper */}
-        <header className="w-full fixed top-0 left-0 p-5 bg-white text-black z-50">
+        <header className="w-full absolute top-0 left-0 p-4 bg-white text-black z-50">
             <nav className="w-full lg:w-1/2 mx-auto flex items-center lg:items-start justify-between">
                 <Link href="/" className="">
                     <h2 className="tracking-widest lg:text-center text-xl font-bold select-none">Hollóvár</h2>
                     <p className="text-md text-center select-none">A felfedezett vár</p>
                 </Link>
-                <div className="flex gap-5 mt-1 items-center">
+                <div className="hidden sm:flex gap-5 mt-1 items-center">
                     <Link href="/programok" className="hover:text-lime-600 transition-colors duration-150 font-light select-none">Programok</Link>
                     <Link href="/nyitvatartas" className="hover:text-lime-600 transition-colors duration-150 font-light select-none">Nyitvatartás</Link>
                     <Link href="/kapcsolat" className="hover:text-lime-600 transition-colors duration-150 font-light select-none">Kapcsolat</Link>
@@ -38,6 +38,11 @@ export default function RootLayout({
         <main className="min-h-screen w-full">
             {children}
         </main>
+
+        {/* footer content */}
+        <footer className="w-full text-center">
+            <p className="text-gray-500">Hollóvár Inc. 2024 &copy; Minden jog fenntartva.</p>
+        </footer>
       </body>
     </html>
   );
--- hollovar/app/page.tsx
+++ hollovar/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className="w-full min-h-screen">
         <div className="w-full h-screen mx-auto pt-48 bg-hero-image bg-cover bg-center bg-no-repeat">
-            <div>
+            <div className="backdrop-blur-sm h-full">
                 <h1 className="animation text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-center mb-3 uppercase tracking-[0.2em]">Fedezd fel a Hollóvárat</h1>
                 <p className="animation font-light text-lg tracking-wider text-center my-8">Sok Kaland és Utazás Vár Rád!</p>
                 <Link href="/programok" className="animation select-none border-2 border-black bg-black text-white tracking-[0.175em] p-3 hover:bg-white hover:text-black transition-colors duration-200 mx-auto block w-max font-light">Programjaink</Link>
@@ -17,7 +17,7 @@ export default function Home() {
                 <p className="text-gray-600 font-light">Tekintse meg alább a bemutatónkat a várról</p>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-                
+
             </div>
         </div>
     </section>
